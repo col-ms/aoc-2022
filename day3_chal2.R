463501596592515b7c69d5106a65f00c9b92414a
Collin Smith <collinmsmith22@gmail.com>
2022-12-06 11:15:34 -0500

    day 3 challenge 2

---
+++ day3_chal2.R
@@ -0,0 +1,19 @@
+data <- readLines("day3_input.txt")
+
+find_dupes <- function(e1, e2, e3){
+  
+  get_chars <- function(str){return(strsplit(str, '')[[1]])}
+  
+  return(intersect(intersect(get_chars(e1), get_chars(e2)), get_chars(e3)))
+}
+
+badges = vector()
+
+i = 1
+
+while(i < 300){
+  badges = append(badges, find_dupes(data[i], data[i+1], data[i+2]))
+  i = i + 3
+}
+
+sum(match(badges, c(letters[1:26], LETTERS[1:26])))
