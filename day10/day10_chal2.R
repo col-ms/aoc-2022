b138a91410a9c732c7b903f4ad36ffe23357c326
Collin Smith <collinmsmith22@gmail.com>
2022-12-19 15:47:31 -0500

    refactored part 2, less calcs per line

--- day10/day10_chal2.R
+++ day10/day10_chal2.R
@@ -1,23 +1,11 @@
 data <- readLines("inputs/day10_input.txt")
-#data <- readLines("day10/test_data.txt")
 
-updateSprite <- function(x){
-  if(x < 0){return(rep('.', 40))}
-  x = x + 1
-  line = rep('.', 40)
-  line[c(x-1,x,x+1)] = "#"
-  return(line[1:40])
+writeScreen <- function(cycle, x){
+  return(ifelse(any(cycle%%40 == c(x-1,x,x+1)), '#', '.'))
 }
 
-writeScreen <- function(cycle, spritePos){
-  return(ifelse(any(cycle%%40 == which(spritePos == '#')), '#', '.'))
-}
-
-csConv <- function(cycle){
-  return(ifelse(cycle%%40 == 0, 40, cycle%%40))
-}
-
-prtSc <- function(screen){
+prtSc <- function(pxls){
+  screen = matrix(pxls, ncol = 40, byrow = TRUE)
   for(i in 1:6){
     print(paste(screen[i,], collapse = ''))
   }
@@ -25,26 +13,25 @@ prtSc <- function(screen){
 
 x <- 1
 cycle <- 0
-screen <- matrix(NA, nrow = 6, ncol = 40)
-spritePos <- updateSprite(x)
+screen <- as.character()
 
 for(l in data){
   
   if(l == "noop"){
+    
+    screen = c(screen, writeScreen(cycle, x))
     cycle = cycle + 1
-    screen[ceiling(cycle/40),csConv(cycle)] = writeScreen(cycle, spritePos)
     
   } else {
     amt = as.numeric(strsplit(l, ' ')[[1]][2])
     
+    screen = c(screen, writeScreen(cycle, x))
     cycle = cycle + 1
-    screen[ceiling(cycle/40),csConv(cycle)] = writeScreen(cycle, spritePos)
     
+    screen = c(screen, writeScreen(cycle, x))
     cycle = cycle + 1
-    screen[ceiling(cycle/40),csConv(cycle)] = writeScreen(cycle, spritePos)
     
     x = x + amt
-    spritePos = updateSprite(x)
   }
 }
 
